28334f2316cd830638685b8a5c785dd78b174328
vahid68 <vmohamadi31@gmail.com>
2023-11-23 07:13:51 -0800

    usermanager

--- MurenoClock/Controllers/AboutController.cs
+++ MurenoClock/Controllers/AboutController.cs
@@ -2,6 +2,7 @@
 using BusinessLayer.Dtos.About;
 using BusinessLayer.Repository;
 using DataLayer.Entities;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using MurenoClock.Controllers.CrudController;
 
@@ -9,9 +10,16 @@ namespace MurenoClock.Controllers;
 
 public class AboutController : Controller/*: CrudController<AboutDto, SelectAboutDto, About, int>*/
 {
-
+    private readonly UserManager<IdentityUser> _user;
+    public AboutController(UserManager<IdentityUser> user)
+    {
+        _user=user;
+    }
+  
     public IActionResult Create()
     {
+        _user.GeneratePasswordResetTokenAsync();
+        _user.pho
         return View();
     }
 
